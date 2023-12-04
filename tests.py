2886cfacc68782ab491f041fe0a9b548cdbb0877
ivanka <ivanka.kozak3@gmail.com>
2023-12-04 22:41:11 +0200

    added more tests, readme info

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import pytest
 from main import app, db
-from models import Category
+from models import Category, Audiobook
 
 # pytest tests.py  - to run the tests
 
@@ -55,3 +55,97 @@ def test_add_category(client):
     assert response.status_code == 201
     data = response.get_json()
     assert 'id' in data
+
+
+def test_get_categories(client):
+    # Adding a category for testing the GET endpoint
+    client.post('/category/', json={'category_name': 'Science'})
+
+    response = client.get('/category/')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list)
+    assert data[0]['category_name'] == 'Science'
+
+
+def test_delete_category(client):
+    # Adding a category to delete
+    post_response = client.post('/category/', json={'category_name': 'Fiction'})
+    category_id = post_response.get_json()['id']
+
+    # Deleting the added category
+    delete_response = client.delete(f'/category/{category_id}')
+    assert delete_response.status_code == 200
+
+    # Verify if category is deleted by trying to fetch it
+    get_response = client.get('/category/')
+    data = get_response.get_json()
+    category = next((cat for cat in data if cat['id'] == category_id), None)
+    assert category is None
+
+
+def test_update_category(client):
+    # Adding a category to update
+    post_response = client.post('/category/', json={'category_name': 'Biography'})
+    category_id = post_response.get_json()['id']
+
+    # Updating the added category
+    update_response = client.put(f'/category/{category_id}', json={'category_name': 'Detective'})
+    assert update_response.status_code == 200
+
+    # Verify if category is updated
+    get_response = client.get('/category/')
+    data = get_response.get_json()
+    updated_category = next((category for category in data if category['id'] == category_id), None)
+    assert updated_category is not None
+    assert updated_category['category_name'] == 'Detective'
+
+
+
+# Utility function to add an audiobook for testing
+def add_test_audiobook(client, title="Test Audiobook", description="A test description", category_id=None):
+    return client.post('/audiobook/', json={
+        'title': title, 
+        'description': description,
+        'category_id': category_id
+    })
+
+# Test the GET endpoint
+def test_get_audiobooks(client):
+    response = client.get('/audiobook/')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list)  # Check if data is a list
+
+# Test the POST endpoint
+def test_add_audiobook(client):
+    response = add_test_audiobook(client)
+    assert response.status_code == 201
+    data = response.get_json()
+    assert 'id' in data
+
+# Test the DELETE endpoint
+def test_delete_audiobook(client):
+    # First add an audiobook to delete
+    add_response = add_test_audiobook(client)
+    audiobook_id = add_response.get_json()['id']
+
+    # Now delete the audiobook
+    del_response = client.delete(f'/audiobook/{audiobook_id}')
+    assert del_response.status_code == 200
+
+# Test the PUT endpoint
+def test_update_audiobook(client):
+    # First add an audiobook to update
+    add_response = add_test_audiobook(client)
+    audiobook_id = add_response.get_json()['id']
+
+    # Now update the audiobook
+    update_data = {
+        'title': 'Updated Title',
+        'description': 'Updated Description',
+    }
+    update_response = client.put(f'/audiobook/{audiobook_id}', json=update_data)
+    assert update_response.status_code == 200
+    updated_data = update_response.get_json()
+    assert updated_data['message'] == "Audiobook updated"
