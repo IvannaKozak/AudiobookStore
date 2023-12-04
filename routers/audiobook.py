3921d0fe1be867afd43d6ec0061cc68495cf0279
ivanka <ivanka.kozak3@gmail.com>
2023-12-04 13:07:46 +0200

    protected all endpoints, added PUT endpoint

--- routers/audiobook.py
+++ routers/audiobook.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, jsonify, request
 
 from models import db, Audiobook
+from routers.auth import token_required
 
 audiobook_bp = Blueprint('audiobook_bp', __name__)
 
 @audiobook_bp.route('/', methods=['GET'])
+@token_required
 def get_audiobooks():
     audiobooks = Audiobook.query.all()
     return jsonify([{'id': ab.id, 'title': ab.title, 'description': ab.description, 'category_id': ab.category_id} for ab in audiobooks])
 
 
 @audiobook_bp.route('/', methods=['POST'])
+@token_required
 def add_audiobook():
     data = request.json
     new_audiobook = Audiobook(
@@ -23,6 +26,7 @@ def add_audiobook():
 
 
 @audiobook_bp.route('/<int:audiobook_id>', methods=['DELETE'])
+@token_required
 def delete_audiobook(audiobook_id):
     audiobook = Audiobook.query.get_or_404(audiobook_id)
     db.session.delete(audiobook)
--- routers/category.py
+++ routers/category.py
@@ -6,6 +6,7 @@ from routers.auth import token_required
 category_bp = Blueprint('category_bp', __name__)
 
 @category_bp.route('/', methods=['POST'])
+@token_required
 def add_category():
     data = request.json
     new_category = Category(category_name=data['category_name'])
@@ -14,6 +15,7 @@ def add_category():
     return jsonify({"message": "Category added", "id": new_category.id}), 201
 
 @category_bp.route('/', methods=['GET'])
+@token_required
 def get_categories():
     categories = Category.query.all()
     category_list = [{'id': category.id, 'category_name': category.category_name} for category in categories]
@@ -27,4 +29,22 @@ def delete_category(category_id):
     db.session.commit()
     return jsonify({"message": "Category deleted"}), 200
 
+
+@category_bp.route('/<int:category_id>', methods=['PUT'])
+@token_required
+def update_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    data = request.get_json()
+    if 'category_name' in data:
+        category.category_name = data['category_name']
+    else:
+        return jsonify({"message": "Missing category_name field"}), 400
+
+    db.session.commit()
+    
+    return jsonify({"message": "Category updated", "id": category.id}), 200
+
+
+
 # eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpdmFua2EiLCJpZCI6MSwicm9sZSI6ImFkbWluIiwiZXhwIjoxNzAxNjA5MTE4fQ.LIJIKBAy_3eSJzQvgyZJsYw3x6TDV0wXMM8lX3wC5NQ
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpdmFua2EiLCJpZCI6MSwicm9sZSI6ImFkbWluIiwiZXhwIjoxNzAxNjg5NjA0fQ.vVcpEe8xaEDyy2ISC5pgcc-S84f53yiD_obpHv6FRjI
